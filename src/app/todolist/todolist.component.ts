8cfb0df1a1d78d8db9ee52dde52697dfcf052f6b
Jesse McIntire <jessemcintire2@gmail.com>
2019-01-17 18:11:54 -0500

    added item delete and modal removal functionality

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Item } from "../_model/Item";
-import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import {
+  NgbModal,
+  ModalDismissReasons,
+  NgbModalRef
+} from "@ng-bootstrap/ng-bootstrap";
 import { ApiService } from "../_services/api.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Router } from "@angular/router";
+import { concat } from "rxjs";
 
 @Component({
   selector: "app-todolist",
@@ -13,6 +18,8 @@ import { Router } from "@angular/router";
 export class TodolistComponent implements OnInit {
   items: Item[];
   closeResult: string;
+  modalReference: NgbModalRef;
+
   static instance: TodolistComponent;
 
   constructor(
@@ -34,6 +41,7 @@ export class TodolistComponent implements OnInit {
 
   onRemove(item) {
     this.service.removeItem(item).then(() => this.updateLocalItems());
+    console.log("Item deleted");
   }
 
   onEdit(item) {
@@ -42,16 +50,14 @@ export class TodolistComponent implements OnInit {
   }
 
   open(content) {
-    this.modalService
-      .open(content, { ariaLabelledBy: "modal-basic-title" })
-      .result.then(
-        result => {
-          this.closeResult = `Closed with: ${result}`;
-        },
-        reason => {
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        }
-      );
+    this.modalService.open(content, { centered: true }).result.then(
+      result => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      reason => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
   }
 
   private getDismissReason(reason: any): string {
@@ -64,36 +70,3 @@ export class TodolistComponent implements OnInit {
     }
   }
 }
-
-// @Component({
-//   selector: "ngbd-modal-basic",
-//   templateUrl: "todolist.component.html"
-// })
-// export class NgbdModalBasic {
-//   closeResult: string;
-
-//   constructor(private modalService: NgbModal) {}
-
-//   open(content) {
-//     this.modalService
-//       .open(content, { ariaLabelledBy: "modal-basic-title" })
-//       .result.then(
-//         result => {
-//           this.closeResult = `Closed with: ${result}`;
-//         },
-//         reason => {
-//           this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-//         }
-//       );
-//   }
-
-//   private getDismissReason(reason: any): string {
-//     if (reason === ModalDismissReasons.ESC) {
-//       return "by pressing ESC";
-//     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-//       return "by clicking on a backdrop";
-//     } else {
-//       return `with: ${reason}`;
-//     }
-//   }
-// }
