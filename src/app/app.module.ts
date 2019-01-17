7cbf66aa95591873844aa248f0ae36439f6f9e4d
Jesse McIntire <jessemcintire2@gmail.com>
2019-01-17 17:17:41 -0500

    first attempt edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { TodolistComponent } from './todolist/todolist.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { NewItemComponent } from './new-item/new-item.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
-import { EditItemComponent } from './edit-item/edit-item.component';
-import { ApiService } from './_services/api.service';
-import { AuthService } from './_services/auth/auth.service';
+import { AppComponent } from "./app.component";
+import { TodolistComponent } from "./todolist/todolist.component";
+import { AppRoutingModule } from ".//app-routing.module";
+import { NewItemComponent } from "./new-item/new-item.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { EditItemComponent } from "./edit-item/edit-item.component";
+import { ApiService } from "./_services/api.service";
+import { AuthService } from "./_services/auth/auth.service";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -26,9 +27,10 @@ import { AuthService } from './_services/auth/auth.service';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [ ApiService, AuthService ],
+  providers: [ApiService, AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,38 +1,99 @@
-import { Component, OnInit } from '@angular/core';
-import { Item } from '../_model/Item';
-import { ApiService } from '../_services/api.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input } from "@angular/core";
+import { Item } from "../_model/Item";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import { ApiService } from "../_services/api.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-todolist',
-  templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  selector: "app-todolist",
+  templateUrl: "./todolist.component.html",
+  styleUrls: ["./todolist.component.css"]
 })
 export class TodolistComponent implements OnInit {
+  items: Item[];
+  closeResult: string;
+  static instance: TodolistComponent;
 
-	items : Item[];
-	static instance: TodolistComponent;
+  constructor(
+    private service: ApiService,
+    private router: Router,
+    private modalService: NgbModal
+  ) {
+    /*private service: HttpItemService*/
 
-	constructor(private service: ApiService, private router: Router) { /*private service: HttpItemService*/
+    this.updateLocalItems();
+  }
 
-		this.updateLocalItems();
-	}
+  updateLocalItems() {
+    console.log("Updating items");
+    this.service.getItems().then(items => (this.items = items));
+  }
 
-	updateLocalItems(){
-		console.log("Updating items");
-		this.service.getItems().then(items => this.items = items);
-	}
+  ngOnInit() {}
 
-  	ngOnInit() {}
+  onRemove(item) {
+    this.service.removeItem(item).then(() => this.updateLocalItems());
+  }
 
-  	onRemove(item){
+  onEdit(item) {
+    this.service.setCurrentItem(item);
+    this.router.navigateByUrl("/edit-item");
+  }
 
-		this.service.removeItem(item).then(() => this.updateLocalItems());
-	}
+  open(content) {
+    this.modalService
+      .open(content, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+        result => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        reason => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
 
-	onEdit(item){
-		this.service.setCurrentItem(item);
-		this.router.navigateByUrl("/edit-item");
-	}
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
+
+// @Component({
+//   selector: "ngbd-modal-basic",
+//   templateUrl: "todolist.component.html"
+// })
+// export class NgbdModalBasic {
+//   closeResult: string;
+
+//   constructor(private modalService: NgbModal) {}
+
+//   open(content) {
+//     this.modalService
+//       .open(content, { ariaLabelledBy: "modal-basic-title" })
+//       .result.then(
+//         result => {
+//           this.closeResult = `Closed with: ${result}`;
+//         },
+//         reason => {
+//           this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+//         }
+//       );
+//   }
+
+//   private getDismissReason(reason: any): string {
+//     if (reason === ModalDismissReasons.ESC) {
+//       return "by pressing ESC";
+//     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+//       return "by clicking on a backdrop";
+//     } else {
+//       return `with: ${reason}`;
+//     }
+//   }
+// }
